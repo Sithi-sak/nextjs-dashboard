94797245f1baeef818549e580ddc2d97d6d27e38
Sithisak Leak <sithisakleak001@gmail.com>
2024-12-04 14:01:19 +0700

    action updated

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -2,7 +2,6 @@
 
 import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
-import { error } from "console";
 import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -106,8 +105,6 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
